e40163d096e61f264b3516b1906ce0841bc3fa73
dwdjjj <dwdjjj@naver.com>
2022-10-11 19:58:35 +0900

    FEAT : button component #5 Button component init & theme update

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,21 @@ import React from 'react';
 import { ThemeProvider } from '@emotion/react';
 import GlobalStyle from './styles/globalStyle';
 import theme from './styles/theme';
+import Button from './components/Button';
 
 const App = () => {
     return (
         <ThemeProvider theme={theme}>
             <GlobalStyle />
-            <h1 style={{ color: theme.color.primary }}>React without CRA</h1>
+            <h1 style={{ color: theme.color.main.dark }}>React without CRA</h1>
+            <div>
+                <Button hoverBgColor="white" hoverColor="gray">
+                    회원가입
+                </Button>
+            </div>
+            <Button color="black" hoverBgColor="white" hoverColor="gray">
+                로그인
+            </Button>
         </ThemeProvider>
     );
 };
