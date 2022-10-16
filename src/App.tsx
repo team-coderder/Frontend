98ff6eea58af61fed1d54e941d3f24964299d5be
dwdjjj <dwdjjj@naver.com>
2022-10-16 15:21:32 +0900

    FEAT : route setup #12

--- src/App.tsx
+++ src/App.tsx
@@ -2,23 +2,16 @@ import React from 'react';
 import { ThemeProvider } from '@emotion/react';
 import GlobalStyle from './styles/globalStyle';
 import theme from './styles/theme';
-import Button from './components/Button';
-import Nav from './components/Nav';
+import Routes from './routes';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 const App = () => {
     return (
         <ThemeProvider theme={theme}>
             <GlobalStyle />
-            <h1 style={{ color: theme.color.main.dark }}>React without CRA</h1>
-            <div>
-                <Button hoverBgColor="white" hoverColor="gray">
-                    회원가입
-                </Button>
-            </div>
-            <Button color="black" hoverBgColor="white" hoverColor="gray">
-                로그인
-            </Button>
-            {/* <Nav url="www.naver.com">naver</Nav> */}
+            <Router>
+                <Routes />
+            </Router>
         </ThemeProvider>
     );
 };
