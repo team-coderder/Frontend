e336ee4a2b21c87a9b1b413035b8eb3c9bc683df
dwdjjj <dwdjjj@naver.com>
2022-10-18 14:51:07 +0900

    FEAT : member component #8 styled component move to outside of function

--- src/components/Member.tsx
+++ src/components/Member.tsx
@@ -5,7 +5,6 @@ interface MemberProps {
     width?: string;
     height?: string;
     fontSize?: 'large' | 'medium' | 'small';
-    fontWeight?: 'bold' | 'normal' | 'thin';
     color?: 'white' | 'black';
     backgroundColor?: 'main' | 'sub';
     radius?: 'large' | 'medium' | 'small';
@@ -14,46 +13,64 @@ interface MemberProps {
     // url: string;
 }
 
+const Component = styled.div<MemberProps>`
+    width: ${(props) => props.width ?? '150px'};
+    height: ${(props) => props.height ?? '40px'};
+    font-size: ${(props) =>
+        ({ theme }) =>
+            !props.fontSize
+                ? theme.font.size.medium
+                : theme.font.size[props.fontSize]}px;
+    color: ${(props) =>
+        ({ theme }) =>
+            !props.color ? theme.color.white : theme.color.black};
+    background-color: ${(props) =>
+        ({ theme }) =>
+            !props.backgroundColor
+                ? theme.color.main.common
+                : theme.color.gray};
+    cursor: pointer;
+    border: none;
+    border-radius: ${(props) =>
+        ({ theme }) =>
+            props.radius === 'small'
+                ? theme.borderRadius.small
+                : props.radius === 'large'
+                ? theme.borderRadius.large
+                : theme.borderRadius.medium};
+    transition: all 0.5s;
+    &:hover {
+        background-color: ${(props) =>
+            ({ theme }) =>
+                !props.focusBgColor
+                    ? props.backgroundColor
+                    : theme.color[props.focusBgColor]};
+    }
+`;
+
 const Member = ({
     width,
     height,
     fontSize,
-    fontWeight,
     color,
     backgroundColor,
     radius,
     focusBgColor,
     children,
 }: MemberProps) => {
-    const Component = styled.div`
-        width: ${width ?? '200px'};
-        height: ${height ?? '50px'};
-        font-size: ${({ theme }) =>
-            !fontSize ? theme.font.size.medium : theme.font.size[fontSize]}px;
-        font-weight: ${({ theme }) =>
-            !fontWeight
-                ? theme.font.weight.normal
-                : theme.font.weight[fontWeight]};
-        color: ${({ theme }) =>
-            !color ? theme.color.white : theme.color.black};
-        background-color: ${({ theme }) =>
-            !backgroundColor ? theme.color.sub.common : theme.color.gray};
-        cursor: pointer;
-        border: none;
-        border-radius: ${({ theme }) =>
-            radius === 'small'
-                ? theme.borderRadius.small
-                : radius === 'large'
-                ? theme.borderRadius.large
-                : theme.borderRadius.medium};
-
-        transition: all 0.5s;
-        &:focus {
-            background-color: ${({ theme }) =>
-                !focusBgColor ? backgroundColor : theme.color[focusBgColor]};
-        }
-    `;
-    return <Component>{children}</Component>;
+    return (
+        <Component
+            width={width}
+            height={height}
+            fontSize={fontSize}
+            color={color}
+            backgroundColor={backgroundColor}
+            radius={radius}
+            focusBgColor={focusBgColor}
+        >
+            {children}
+        </Component>
+    );
 };
 
 export default Member;
