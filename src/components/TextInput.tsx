767e9879f5e686277a4db329359b3eaab18866f2
dwdjjj <dwdjjj@naver.com>
2022-10-18 14:34:27 +0900

    FEAT : text input component #7 focus error solved

--- src/components/TextInput.tsx
+++ src/components/TextInput.tsx
@@ -14,29 +14,30 @@ interface TextInputProps {
     onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
+const Component = styled.input<TextInputProps>`
+    width: ${(props) => props.width ?? 'auto'};
+    height: ${(props) => props.height ?? 'auto'};
+    margin: ${(props) => props.margin ?? '0'};
+    color: ${({ theme }) => theme.color.black};
+    border-bottom: ${({ theme }) => theme.color.gray} 1px solid;
+    border-left: medium none;
+    border-right: medium none;
+    border-top: medium none;
+    &:focus {
+        outline: none;
+        border-bottom: 1px solid ${({ theme }) => theme.color.black};
+    }
+`;
+
+const Label = styled.label`
+    display: flex;
+    flex-direction: column;
+    font-size: ${({ theme }) => theme.font.size.small}px;
+`;
+
 function TextInput(props: TextInputProps) {
     const theme = useTheme();
 
-    const Component = styled.input`
-        width: ${props.width ?? 'auto'};
-        height: ${props.height ?? 'auto'};
-        margin: ${props.margin ?? '0'};
-        color: ${theme.color.black};
-        border-bottom: ${theme.color.gray} 1px solid;
-        border-left: medium none;
-        border-right: medium none;
-        border-top: medium none;
-        &:focus {
-            outline: none;
-            border-bottom: 1px solid ${theme.color.black};
-        }
-    `;
-
-    const Label = styled.label`
-        display: flex;
-        flex-direction: column;
-        font-size: ${({ theme }) => theme.font.size.small}px;
-    `;
     return (
         <Label>
             <Component
