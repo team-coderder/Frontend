cb9808558c7036c7abc49141f1b8c4f43eac02e5
dwdjjj <dwdjjj@naver.com>
2022-11-03 09:45:42 +0900

    FEAT : Signup View #21 init route

--- src/routes/mainRoutes.tsx
+++ src/routes/mainRoutes.tsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import Signup from '../views/Signup';
 
 const mainRoutes = () => {
     return {
         path: '/',
-        children: [{ path: '' }],
+        children: [
+            { path: '' },
+            {
+                path: '/signup',
+                element: <Signup />,
+            },
+        ],
     };
 };
 
