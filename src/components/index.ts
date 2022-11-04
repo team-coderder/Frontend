42a3341c22d15657c7fae984b575ce48234bff2e
dwdjjj <dwdjjj@naver.com>
2022-11-05 00:18:58 +0900

    Revert "FEAT : Add Group View #22 pull develop branch"
    This reverts commit e10ef013997e0a133a13219547d1098bdc2f3d83, reversing
    changes made to ec5ee640e23f9788e54aba1b37f9e6d3ce6053ef.

--- src/components/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as Bar } from './Bar';
-export { default as Modal } from './Modal';
-export { default as Navbar } from './Navbar';
--- src/routes/index.ts
+++
@@ -1,7 +0,0 @@
-import { useRoutes } from 'react-router-dom';
-
-import mainRoutes from './mainRoutes';
-
-export default function ThemeRoutes() {
-    return useRoutes([mainRoutes()]);
-}
