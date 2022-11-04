5c5f17a0d384f74ff01c67fbb35e6521748dcd10
lim-jiwoo <il04131@naver.com>
2022-11-05 00:50:31 +0900

    FEAT : groupbar component #10 add exports at index file

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,3 +2,5 @@ export { default as Bar } from './Bar';
 export { default as Modal } from './Modal';
 export { default as Navbar } from './Navbar';
 export { default as Groupbar } from './Groupbar';
+export { default as TextInput } from './TextInput';
+export { default as Button } from './Button';
