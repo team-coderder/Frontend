a859e89eb72579ab92b4e4e236c0dbe7d4cad5de
lim-jiwoo <il04131@naver.com>
2022-11-12 00:23:04 +0900

    STYLE : groupinfo view #23 apply minor changes to groupinfo style

--- src/views/GroupInfo.tsx
+++ src/views/GroupInfo.tsx
@@ -8,102 +8,54 @@ const Container = styled.div`
     margin: auto;
     margin-top: 150px;
 `;
+const Header = styled.div`
+    color: #fff;
+`;
 const Main = styled.div`
     margin-top: 50px;
 `;
 const Field = styled.div`
+    height: 2.5rem;
     display: flex;
-    margin: 1rem 0;
+    align-items: center;
+    margin: 20px 0;
 `;
 const FieldName = styled.div`
-    line-height: 2rem;
-    text-align: center;
+    color: #fff;
     width: 100px;
-    text-align: left;
 `;
-
-const _users = [
-    {
-        "user_id" : "1",
-        "username": "molisee123",
-        "nickname": "molisee"
-    },
-    {
-        "user_id" : "2",
-        "username": "tousles22",
-        "nickname": "jours"
-    },
-];
-const _team_members = [
-    {
-        "memberId": 2,
-        "username": "gildong",
-        "nickname": "홍길동",
-        "teamRole": "FOLLOWER"
-    },
-    {
-        "memberId": 3,
-        "username": "bata",
-        "nickname": "바타",
-        "teamRole": "FOLLOWER"
-    },
-];
 
 const GroupInfo = () => (
     <Container>
-        <h1>그룹 정보 수정</h1>
+        <Header>
+            <h1>그룹 정보 수정</h1>
+        </Header>
         <Main>
             <Field>
                 <FieldName>그룹 이름</FieldName>
-                <TextInput height="100%" width="60em" />
-                <Button height="2em" width="9em">
+                <TextInput width="700px" height="2rem" margin="0 20px 0 0" />
+                <Button height="100%" width="9em">
                     수정하기
                 </Button>
             </Field>
             <Field>
                 <FieldName>멤버 관리</FieldName>
-                <TextInput height="100%" width="60em" placeholder="ID 검색">
-                    {/* <Field>
-                        {_users?.map((user, index) => (
-                            <>
-                                <input
-                                    key={index}
-                                    disabled
-                                    value={user.username}
-                                />
-                                <Button height="2em" width="9em">
-                                    추가하기
-                                </Button>
-                            </>
-                        ))}
-                    </Field> */}
-                </TextInput>
+                <TextInput width="700px" height="2rem" placeholder="ID 검색" />
             </Field>
             <Field>
                 <FieldName></FieldName>
-                {_team_members?.map((member, index) => (
-                    <>
-                        <Member
-                            key={index}
-                            backgroundColor="main"
-                            height="2em"
-                            width="7em"
-                            space={5}
-                        >
-                            {member.username}
-                        </Member>
-                    </>
-                ))}
+                <Member backgroundColor="main" height="100%" width="7em" space={5}>홍길동</Member>
+                <Member backgroundColor="main" height="100%" width="7em" space={5}>아바타</Member>
             </Field>
             <Field>
                 <FieldName>그룹 삭제</FieldName>
-                <Button height="2em" width="9em">
+                <Button height="100%" width="9em">
                     그룹 삭제
                 </Button>
             </Field>
             <Field>
                 <FieldName>그룹 탈퇴</FieldName>
-                <Button height="2em" width="9em">
+                <Button height="100%" width="9em">
                     그룹 탈퇴
                 </Button>
             </Field>
