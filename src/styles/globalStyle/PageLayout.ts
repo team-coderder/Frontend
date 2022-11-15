39565557550dcaad8dedf35c9ffee42c74099ec6
lim-jiwoo <il04131@naver.com>
2022-11-15 13:21:51 +0900

    STYLE : teamschedule view #27 move styled components to styles folder

---
+++ src/styles/globalStyle/PageLayout.ts
@@ -0,0 +1,40 @@
+import styled from '@emotion/styled';
+
+export const Container = styled.div`
+    width: 80%;
+    margin: auto;
+    margin-top: 150px;
+`;
+
+export const Header = styled.div`
+    color: #fff;
+    display: flex;
+    align-items: center;
+`;
+
+export const SubHeader = styled.div`
+    color: #fff;
+    align-items: center;
+    font-size: 1.2rem;
+    margin-right: 50px;
+`;
+
+export const Main = styled.div`
+    margin: 50px 0;
+`;
+
+export const Field = styled.div`
+    margin: 15px 0 20px 0;
+    display: flex;
+    align-items: center;
+`;
+
+export const AlignRight = styled.div`
+    display: flex;
+    justify-content: flex-end;
+`;
+
+export const MarginRight = styled.div`
+    margin-right: 10px;
+    display: inline-block;
+`;
--- src/views/TeamSchedule.tsx
+++ src/views/TeamSchedule.tsx
@@ -1,70 +1,51 @@
 import React from 'react';
-import styled from '@emotion/styled';
 import { FiSettings } from 'react-icons/fi';
 
 import { Button, Member } from '../components';
-
-const Container = styled.div`
-    width: 80%;
-    margin: auto;
-    margin-top: 150px;
-`;
-const Header = styled.div`
-    color: #fff;
-    display: flex;
-    align-items: center;
-    & svg {
-        display: block;
-        margin-left: 10px;
-        cursor: pointer;
-    }
-`;
-const AlignRight = styled.div`
-    display: flex;
-    justify-content: flex-end;
-`;
-const Schedule = styled.div`
-    height: 300px;
-    background-color: #eee;
-    margin: 20px 0;
-`;
-const Field = styled.div`
-    margin-bottom: 20px;
-    & h2 {
-        margin-bottom: 8px;
-        color: #fff;
-        font-size: 1.2rem;
-    }
-`;
-const MarginRight = styled.div`
-    margin-right: 10px;
-    display: inline-block;
-`;
+import { Container, Header, SubHeader, AlignRight, Field, MarginRight } from '../styles/globalStyle/PageLayout';
+import { Schedule } from '../styles/schedule/schedule';
 
 const TeamSchedule = () => {
-  return (
+    return (
         <Container>
             <Header>
                 <h1>그룹 이름</h1>
-                <FiSettings size="24" />
+                <FiSettings
+                    size="24"
+                    style={{ marginLeft: '10px', cursor: 'pointer' }}
+                />
             </Header>
             <AlignRight>
                 <Button height="2.5rem">그룹 스케줄 수정</Button>
             </AlignRight>
             <Schedule />
+            <SubHeader>그룹원</SubHeader>
             <Field>
-                <h2>그룹원</h2>
                 <Member backgroundColor="main">나</Member>
             </Field>
+            <SubHeader>보기모드</SubHeader>
             <Field>
-                <h2>보기모드</h2>
                 <MarginRight>
-                    <Button width="5rem" height="2.5rem" color="black" backgroundColor='gray'>색깔로</Button>
+                    <Button
+                        width="5rem"
+                        height="2.5rem"
+                        color="black"
+                        backgroundColor="gray"
+                    >
+                        색깔로
+                    </Button>
                 </MarginRight>
-                <Button width="5rem" height="2.4rem" color="black" backgroundColor='gray'>진하게</Button>
+                <Button
+                    width="5rem"
+                    height="2.4rem"
+                    color="black"
+                    backgroundColor="gray"
+                >
+                    진하게
+                </Button>
             </Field>
         </Container>
-  )
+    );
 };
 
 export default TeamSchedule;
