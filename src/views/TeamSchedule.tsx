a59822a6093f349e3ae956f0bbb11cd625bca629
dwdjjj <dwdjjj@naver.com>
2022-11-17 15:45:35 +0900

    FEAT : Main Schedule View #34 init schedule view

--- src/views/TeamSchedule.tsx
+++ src/views/TeamSchedule.tsx
@@ -2,8 +2,15 @@ import React from 'react';
 import { FiSettings } from 'react-icons/fi';
 
 import { Button, Member } from '../components';
-import { Container, Header, SubHeader, AlignRight, Field, MarginRight } from '../styles/globalStyle/PageLayout';
-import { Schedule } from '../styles/schedule/schedule';
+import {
+    Container,
+    Header,
+    SubHeader,
+    AlignRight,
+    Field,
+    MarginRight,
+} from '../styles/globalStyle/PageLayout';
+import { DayPilot, DayPilotCalendar } from '@daypilot/daypilot-lite-react';
 
 const TeamSchedule = () => {
     return (
@@ -18,7 +25,12 @@ const TeamSchedule = () => {
             <AlignRight>
                 <Button height="2.5rem">그룹 스케줄 수정</Button>
             </AlignRight>
-            <Schedule />
+            <DayPilotCalendar
+                durationBarVisible={false}
+                viewType="day"
+                days={7}
+                startDate={DayPilot.Date.today()}
+            />
             <SubHeader>그룹원</SubHeader>
             <Field>
                 <Member backgroundColor="main">나</Member>
